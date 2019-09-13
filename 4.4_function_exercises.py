f60df5bbfb4010c2200612175339bb35a5e13eb0
soslin <seanoslin@gmail.com>
2019-09-12 20:09:51 -0500

    Update

--- 4.4_function_exercises.py
+++ 4.4_function_exercises.py
@@ -15,7 +15,7 @@ def is_vowel(letter):
     else:
         return False
 
-# Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. 
+# 3. Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. 
 # Use your is_vowel function to accomplish this.
 
 def is_consonant(letter):
@@ -26,7 +26,7 @@ def is_consonant(letter):
         return True
 
 
-# Define a function that accepts a string that is a word. The function should 
+# 4.Define a function that accepts a string that is a word. The function should 
 # capitalize the first letter of the word if the word starts with a consonant.
 
 def accepts_string(word):
@@ -34,9 +34,32 @@ def accepts_string(word):
     if word[0] not in vowels:
         return word[0].upper()+word[1:]
 
-# Define a function named calculate_tip. It should accept a tip 
+# 5. Define a function named calculate_tip. It should accept a tip 
 # percentage (a number between 0 and 1) and the bill total, and return the amount to tip.
 
 def calculate_tip(amount):
-    bill_total = 0
-    
+    tip_percent = .2*(amount)
+    return amount + tip_percent
+
+# 6. Define a function named apply_discount. It should 
+# accept a original price, and a discount percentage, and return the price after the discount is applied
+
+def apply_discount(amount, discount):
+    return amount - amount*discount
+
+# 7. Define a function named handle_commas. It should accept a string that is a number that contains 
+# commas in it as input, and return a number as output.
+
+def handle_commas(amount):
+    comma_free_amount = int(amount.replace(",",""))
+    return comma_free_amount
+
+
+# Define a function named get_letter_grade. It should accept a number and return the letter grade 
+# associated with that number (A-F)
+def get_letter_grade(letter):
+    grades = {4:"A", 3:"B", 2:"C", 1:"D", 0:"F"}
+    return grades
+
+
+
