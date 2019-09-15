bc0fd9f21112c5b22607b1f277a2cee2039211ca
soslin <seanoslin@gmail.com>
2019-09-15 10:09:13 -0500

    last update

--- list_comprehension_exercises.py
+++ list_comprehension_exercises.py
@@ -71,30 +71,109 @@ print([fruit for fruit in fruits if len(fruit) > 5])
 
 # 8. Make a list containing the number of characters in each fruit. Output would be [5, 4, 10, etc... ]
 characters_per_fruit = []
-for count(fruit) in fruits:
-    characters_per_fruit += fruit
-    print(characters_per_fruit)
+for fruit in fruits:
+    cnt = len(fruit)
+    characters_per_fruit.append(cnt)
+print(characters_per_fruit)
 
+characters_per_fruit = [len(fruit) for fruit in fruits ]
+print(characters_per_fruit)
 
 # Exercise 9 - Make a variable named fruits_with_letter_a that 
 # contains a list of only the fruits that contain the letter "a"
 
 fruits_with_letter_a = []
 for f in fruits:
-    if f in 'a':
+    if 'a' in f:
         fruits_with_letter_a.append(f)
-    print (fruits_with_letter_a)
+print(fruits_with_letter_a)
 
-
+fruits_with_letter_a = [f for f in fruits if 'a' in f]
+print(fruits_with_letter_a)
 
 # Exercise 10 - Make a variable named even_numbers that holds only the even numbers
-numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers = [-5,-3,-4,-10,1,2,3,4,5,6,7,8,9,10,12,99,-34,101]
 
 list_of_evens = []
 for n in numbers:
     if n % 2 == 0:
-        list_of_evens += n
-    print( list_of_evens)
+        list_of_evens.append(n)
+print(list_of_evens)
+
+list_of_evens = [n for n in numbers if n % 2 == 0]
+print(list_of_evens)
+
+# Exercise 11 - Make a variable named odd_numbers that holds only the odd numbers
+odd_numbers = []
+for n in numbers:
+    if n % 2 != 0:
+        odd_numbers.append(n)
+print(odd_numbers)
+
+odd_numbers = [n for n in numbers if n % 2 != 0]
+print(odd_numbers)
 
 
-# Exercise 11
+# Exercise 12 - Make a variable named positive_numbers that holds only the positive numbers
+pos_nums = []
+for n in numbers:
+    if n >= 0:
+        pos_nums.append(n)
+print(pos_nums)
+
+pos_nums = [n for n in numbers if n >= 0]
+print(pos_nums)
+
+
+# Exercise 13 - Make a variable named negative_numbers that holds only the negative numbers
+neg_nums = []
+for n in numbers:
+    if n <= 0:
+        neg_nums.append(n)
+print(neg_nums)
+
+neg_nums = [n for n in numbers if n <= 0]
+print(neg_nums)
+
+# Exercise 14 - use a list comprehension w/ a conditional in order to produce a list 
+# of numbers with 2 or more numerals
+
+list_w_two_or_more_numerals = []
+for n in numbers:
+    if n > 9 or n <-9:
+        list_w_two_or_more_numerals.append(n)
+print(list_w_two_or_more_numerals)
+
+list_w_two_or_more_numerals = [n for n in numbers if n > 9 or n < -9]
+print(list_w_two_or_more_numerals)
+
+# Exercise 15 - Make a variable named numbers_squared that contains the numbers list 
+# with each element squared. Output is [4, 9, 16, etc...]
+number_squared = []
+for n in numbers: 
+    print(n**2)
+
+number_squared = [n**2 for n in numbers]
+print(number_squared)
+
+# Exercise 16 - Make a variable named odd_negative_numbers that contains only the numbers 
+# that are both odd and negative.
+
+odd_negative_numbers = []
+for n in numbers:
+    if n < 0 and n % 2!= 0:
+        odd_negative_numbers.append(n)
+print(odd_negative_numbers)
+
+odd_negative_numbers = [n for n in numbers if n < 0 and n % 2!= 0]
+print(odd_negative_numbers)
+
+# Exercise 17 - Make a variable named numbers_plus_5. In it, return a list containing 
+# each number plus five. 
+numbers_plus_5 = []
+for n in numbers:
+    numbers_plus_5.append(n+5)
+print(numbers_plus_5)
+
+numbers_plus_5 = [n+5 for n in numbers]
+print(numbers_plus_5)
