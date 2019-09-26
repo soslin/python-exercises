5d0e3b08a2d176e71bd21e7c886b59de59e1eb18
soslin <seanoslin@gmail.com>
2019-09-26 16:52:18 -0500

    Pandas exercises

---
+++ pandas_adv_df.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+np.random.seed(123)
+
+students = ['Sally', 'Jane', 'Suzie', 'Billy', 'Ada', 'John', 'Thomas',
+            'Marie', 'Albert', 'Richard', 'Isaac', 'Alan']
+
+# randomly generate scores for each student for each subject
+# note that all the values need to have the same length here
+math_grades = np.random.randint(low=60, high=100, size=len(students))
+english_grades = np.random.randint(low=60, high=100, size=len(students))
+reading_grades = np.random.randint(low=60, high=100, size=len(students))
+
+df = pd.DataFrame({'name': students,
+                   'math': math_grades,
+                   'english': english_grades,
+                   'reading': reading_grades,
+                   'classroom': np.random.choice(['A', 'B'], len(students))})
